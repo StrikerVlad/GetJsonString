62b4abd7a66ca574f4acdeba1397985220dc088d
Владислав Труфанов <@local.itstep>
2019-02-13 17:58:08 +0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp1.model;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ServiceUser serviceUser = new ServiceUser("https://randomuser.me/api?results=1");
+            Console.WriteLine(serviceUser.GetJsonString()); 
+
+
+        }
+
+        //static void task1()
+        //{
+        //    Console.WriteLine("Write your name");
+        //    string name = Console.ReadLine();
+        //    Console.WriteLine("Hello, {0}", name);
+        //}
+
+        //static void task2()
+        //{
+        //    //int a = Int32.Parse(Console.ReadLine());
+        //    //int b = Int32.Parse(Console.ReadLine());
+        //    //Console.WriteLine(a + b);
+        //    int y = 0;
+        //    int temp = 0;
+        //    while(Int32.TryParse(Console.ReadLine(), out y))
+        //    {
+        //        temp += y;
+        //    }
+
+        //    Console.WriteLine(temp);
+        //}
+
+        //static void task3()
+        //{
+        //    Console.WriteLine("{0} {1} {2}", 1,2,3);
+        //}
+
+
+        //static void task4()
+
+        //{
+        //    int cm = Int32.Parse(Console.ReadLine());
+        //    double m = cm / 100;
+        //    Console.WriteLine("m = {}", m);
+        //}
+
+        //static void task5()
+
+        //{
+        //    bool a = true, b = false, c = false;
+        //    if(a||)
+        //    {
+
+        //    }
+        //}
+
+        //static void task6()
+        //{
+        //    int[] a = new int[5];
+        //    double[,] b  = new double[3, 4];
+        //    Random rnd = new Random();
+        //    int maxA = 0;
+        //    double maxB = 0;
+        //    for (int i = 0; i < a.Length; i++)
+        //    {
+        //        a[i] = rnd.Next(0, 10);
+        //        Console.Write("{0} ", a[i]);
+        //        if (a[i] > maxA)
+        //            maxA = a[i];
+        //    }
+        //    Console.WriteLine();
+
+        //    for (int i = 0; i<3; i++)
+        //    {
+        //        for (int j = 0; j < 4; j++)
+        //        {
+        //            b[i,j] = rnd.NextDouble();
+        //            Console.Write("{0} ", b[i, j]);
+        //            if (b[i, j] > maxB)
+        //                maxB = b[i, j];
+        //        }
+        //        Console.WriteLine();
+        //    }
+
+        //  maxA>maxB?Console.Write("{0}", MaxA)): MaxB;
+        //}
+       
+
+        
+    }
+}
---
+++ ConsoleApp1/model/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.model
+{
+    public enum RequestType
+    {
+        SALES,
+        SALESIMS,
+        STORED,
+        MOVING,
+        GOODS_RECEIPT
+    }
+    public class Request
+    {
+        public Request()
+        {
+
+        }
+
+        public Request(DateTime Data1, DateTime Data2)
+        {
+            this.Data1 = Data1;
+            this.Data2 = Data2;
+        }
+        public RequestType RequestType { get; set; }
+        /// <summary>
+        /// Код поставщика из системы SAP ERP (или BIN).
+        /// </summary>
+        public int KUNNR { get; set; }
+        /// <summary>
+        /// Код БЕ в SAP ERP. Код БЕ может быть заполнен, а может и не заполнен. 
+        /// </summary>
+        public int Burks { get; set; }
+        /// <summary>
+        /// Завод, в рамках которого необходимо передать данные по продажам из системы SAP ERP. 
+        /// </summary>
+        public int Werks { get; set; }
+        /// <summary>
+        /// Дата с начало периода
+        /// </summary>
+        public DateTime Data1 { get; set; }
+        /// <summary>
+        /// Дата с начало периода
+        /// </summary>
+        public DateTime Data2 { get; set; }
+        
+
+    }
+}
